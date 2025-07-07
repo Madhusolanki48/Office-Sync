29035f69acced4831a375d2eb2be0dbe15b81a65
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-07-07 23:52:51 +0530

    added emp details table

--- offc_emp_proj/emp_app/views.py
+++ offc_emp_proj/emp_app/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Employee,Role,Department
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def all_emp(request):
-    return render(request, 'view_all_emp.html')
+    emps=Employee.objects.all()
+    context={
+        'emps':emps
+    }
+    print(context)
+    return render(request, 'view_all_emp.html',context)
 
 def add_emp(request):
     return render(request, 'add_emp.html')
