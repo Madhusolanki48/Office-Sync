0a1f71547ff9e477538c3e687ef4332051821080
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-07-08 01:30:17 +0530

    feat: add emp form and POST request handling

--- offc_emp_proj/emp_app/views.py
+++ offc_emp_proj/emp_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Employee,Role,Department
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,22 @@ def all_emp(request):
     return render(request, 'view_all_emp.html',context)
 
 def add_emp(request):
-    return render(request, 'add_emp.html')
+    if request.method=='POST':
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        salary=int(request.POST['salary'])
+        bonus=int(request.POST['bonus'])
+        dept=int(request.POST['dept'])
+        phone=int(request.POST['phone'])
+        role=int(request.POST['role'])
+        new_emp=Employee(first_name=first_name,last_name=last_name,salary=salary,bonus=bonus,
+                 phone=phone,dept_id=dept,role_id=role,hire_date=datetime.now())
+        new_emp.save()
+        return HttpResponse('Employee added Successfully!')
+    elif request.method=='GET':
+        return render(request,'add_emp.html')
+    else:
+        return HttpResponse('An Exception Occured! Employee has not been added')
 
 def remove_emp(request):
     return render(request, 'remove_emp.html')
